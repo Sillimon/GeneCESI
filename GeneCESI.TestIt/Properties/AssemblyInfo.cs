c9dc3738fd12be8a29596ee7421ccef36c7592f9
Tutu <murier.simon@gmail.com>
2020-05-27 15:45:24 +0200

    Test project

---
+++ GeneCESI.TestIt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GeneCESI.TestIt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeneCESI.TestIt")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6de84c82-1e90-4cae-8886-233ea996a9ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GeneCESI.TestIt/UserCRUD.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using GeneCESI.Lib.Objects;
+using GeneCESI.Lib.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeneCESI.TestIt
+{
+    [TestClass]
+    public class UserCRUD
+    {
+        static IRepository<User> _userRepo = new UserRepository(new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30"));
+
+        [TestMethod]
+        public bool UserAdd()
+        {
+            var newUser = new User("Einaudi", "Ludovico", true, "ludo.einaudi@gmail.com", "somePassword");
+
+            try { _userRepo.Insert(newUser); } catch { return false; }
+
+            return true;
+        }
+
+        [TestMethod]
+        public bool UserDelete()
+        {
+            var user = new User();
+            user.Id = 1;
+            try { _userRepo.Delete(user); } catch { return false; }
+
+            return true;
+        }
+
+        [TestMethod]
+        public static bool UserGetById()
+        {
+            var user = new User();
+
+            try { user = _userRepo.GetById(1); } catch { return false; }
+
+            return true;
+        }
+
+        [TestMethod]
+        public static bool UserGetAll()
+        {
+            var users = new List<User>();
+
+            try { users = _userRepo.GetAll().ToList<User>(); } catch { return false; }
+
+            foreach (var u in users)
+                Console.WriteLine(u.ToString());
+
+            return true;
+        }
+    }
+}
