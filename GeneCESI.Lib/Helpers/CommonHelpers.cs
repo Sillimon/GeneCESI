92a561484c49d0d89348fdb0d1f724683dde4abb
Tutu <murier.simon@gmail.com>
2020-05-27 16:28:54 +0200

    CommonHelpers class with StatementsToString function

---
+++ GeneCESI.Lib/Helpers/CommonHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneCESI.Lib.Helpers
+{
+    public static class CommonHelpers
+    {
+        public static List<string> StatementsToString(string statements) => statements.Split(';').ToList<string>();
+    }
+}
