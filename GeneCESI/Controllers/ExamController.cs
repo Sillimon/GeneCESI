b3daad5d9ac8411b3e62e7086bbd5c2217f2dd73
guldance <loic.guldenfels@gmail.com>
2020-05-27 17:09:40 +0200

    Add DisplayExam view

--- GeneCESI/Controllers/ExamController.cs
+++ GeneCESI/Controllers/ExamController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GeneCESI.Lib.Objects;
 
 namespace GeneCESI.Controllers
 {
     public class ExamController : Controller
     {
+        Exam exam = new Exam("Mon premier examen", 5, 30, new DateTime(2020, 11, 21), 2, 0);
+        List<Question> questions = new List<Question>();
         // GET: Exam
         public ActionResult Evaluation()
         {
-            return View();
+            //Create an examen
+            return View(questions);
+        }
+
+        public ActionResult DisplayExam(int idExamen)
+        {
+            questions.Add(new Question("Est-ce que l'interface est belle ?", Lib.Helpers.EnumHelper.QuestionType.YesNo,
+            new Answer("Yes", "Yes;No", 0), exam));
+            questions.Add(new Question("Question à choix multiples ?", Lib.Helpers.EnumHelper.QuestionType.MultipleChoice,
+                new Answer("reponse D", "reponse A;reponse B;reponse C;reponse D"),exam));
+            return View(questions);
         }
 
         public ActionResult CreationExamen()
