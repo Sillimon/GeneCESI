f0c51d337889678702e5a8c3807e8943fd422831
Tutu <murier.simon@gmail.com>
2020-05-28 16:41:25 +0200

    DB script --> user password length 50 to 250

--- DatabaseQuery.sql
+++ DatabaseQuery.sql
@@ -20,7 +20,7 @@ CREATE TABLE [dbo].[Users] (
     [Firstname] VARCHAR (50)  NOT NULL,
     [Roles]     BIT           NOT NULL,
     [Email]     VARCHAR (MAX) NOT NULL,
-    [Password]  VARCHAR (50)  NOT NULL,
+    [Password]  VARCHAR (250)  NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
