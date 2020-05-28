4a1172577aa5da5322cf47c9d62db655709e68d3
guldance <loic.guldenfels@gmail.com>
2020-05-28 10:09:20 +0200

    Vue de remplissage d'un examen

--- GeneCESI/Controllers/ExamController.cs
+++ GeneCESI/Controllers/ExamController.cs
@@ -11,20 +11,21 @@ namespace GeneCESI.Controllers
     {
         Exam exam = new Exam("Mon premier examen", 5, 30, new DateTime(2020, 11, 21), 2, 0);
         List<Question> questions = new List<Question>();
-        // GET: Exam
-        public ActionResult Evaluation()
-        {
-            //Create an examen
-            return View(questions);
-        }
 
-        public ActionResult DisplayExam(int idExamen)
+        public ActionResult DisplayExam()
         {
             questions.Add(new Question("Est-ce que l'interface est belle ?", Lib.Helpers.EnumHelper.QuestionType.YesNo,
             new Answer("Yes", "Yes;No", 0), exam));
             questions.Add(new Question("Question à choix multiples ?", Lib.Helpers.EnumHelper.QuestionType.MultipleChoice,
                 new Answer("reponse D", "reponse A;reponse B;reponse C;reponse D"),exam));
             return View(questions);
+        }
+
+        public void SendExam_OnButtonClick()
+        {
+            //Récuperer les données de la view
+            //Envoyer les réponses a l'examen sur la BDD
+            //Renvoyer une view qui indique que l'examen a bien été posté.
         }
 
         public ActionResult CreationExamen()
