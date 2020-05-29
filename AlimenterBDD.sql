58e3b12f28ee43434ae6c8dd39b2a1fe9f5a064d
srg-k <srghunterjeu@gmail.com>
2020-05-29 20:47:27 +0200

    (fixed) script for database

--- AlimenterBDD.sql
+++ AlimenterBDD.sql
@@ -1,5 +1,5 @@
 ﻿Insert into dbo.[Users] (Name, Firstname, Roles, Email, Password) values("SergeAdmin","KOPP",True,"un@Mail.fr","SERGE");
 Insert into dbo.[Users] (Name, Firstname, Roles, Email, Password) values("SergeUser","KOPP",False,"deux@Mail.fr","SERGE");
 INSERT INTO dbo.[Questions](Id_Answers, Id_Exams, Label, Type) " +"VALUES(1, 1, "Une question?(1)", 1)
-INSERT INTO dbo.[Questions](Id_Answers, Id_Exams, Label, Type) " +"VALUES(1, 1, "Une question?(2)", 2)
-INSERT INTO dbo.[Questions](Id_Answers, Id_Exams, Label, Type) " +"VALUES(1, 1, "Une question?(3)", 3)
+INSERT INTO dbo.[Questions](Id_Answers, Id_Exams, Label, Type) " +"VALUES(2, 1, "Une question?(2)", 2)
+INSERT INTO dbo.[Questions](Id_Answers, Id_Exams, Label, Type) " +"VALUES(3, 1, "Une question?(3)", 3)
