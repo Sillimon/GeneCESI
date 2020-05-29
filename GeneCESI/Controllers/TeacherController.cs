50b25e5e01efddef7f30380c1d104a63a17e7607
Tutu <murier.simon@gmail.com>
2020-05-29 17:06:15 +0200

    mdf searched in AppData\Roaming

--- GeneCESI/Controllers/TeacherController.cs
+++ GeneCESI/Controllers/TeacherController.cs
@@ -3,6 +3,7 @@ using GeneCESI.Lib.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +14,9 @@ namespace GeneCESI.Controllers
     {
         public ActionResult Index()
         {
-            ExamRepository repoExam = new ExamRepository(new SqlConnection(@"Data Source=(LocalDb)\MSSQLLocalDb;Initial Catalog=geneCesi2;Integrated Security=True;Pooling=False"));
+            ExamRepository repoExam = new ExamRepository(new SqlConnection(@"Data Source = (LocalDb)\MSSQLLocalDB;  Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False; AttachDbFilename=" +
+           @"C:\Users\murie\AppData" +
+           @"\GeneCESI_BDD.mdf;"));
             List<Exam> exams = repoExam.GetAll().ToList();
             return View(exams);
         }
